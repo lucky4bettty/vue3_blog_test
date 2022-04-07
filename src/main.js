84de6255b7569bf1845622f7cc17e2c57c837bf3
betty <xu3ru8u6@i-mps.com>
2022-04-07 16:43:22 +0800

    新增ui-element

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import VueAxios from 'vue-axios'
 import $ from 'jquery';
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ElementPlus from 'element-plus'
+import zhTw from 'element-plus/es/locale/lang/zh-tw'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 window.jQuery = window.$ = $;
-app.use(store).use(router).use(VueAxios, axios).mount("#app");
+app.use(ElementPlus, {locale: zhTw}).use(store).use(router).use(VueAxios, axios).mount("#app");
--- src/views/user/Home.vue
+++ src/views/user/Home.vue
@@ -1,25 +1,89 @@
 <template>
   <div class="home">
-      <div class="article_all">
+      <div class="home_cont">
         <h2>XX文章</h2>
         <!-- 多筆ＬＩＳＴ -->
-        <div>
+        <div class="article_all">
           <!-- 單筆start -->
-          <div class="article">
-            <div>我是標題</div>
-            <div>我是內容</div>
-            <span>作者</span> . <span>發布日期</span>
+          <div class="article_one">
+            <!-- 收藏 -->
+            <div class="article_like">
+              <div class="num">
+                0
+              </div>
+              <div class="word">
+                like
+              </div>
+            </div>
+            <!-- 回覆 -->
+            <div class="article_reply">
+              <div class="num">
+                0
+              </div>
+              <div class="word">
+                回答
+              </div>
+            </div>
+            <!-- 文章 -->
+            <div class="article">
+              <div class="article_title">我是標題</div>
+              <!-- <div class="article_cont">
+                我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容
+                我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容
+                我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容我是內容
+              </div> -->
+              <span class="article_author">作者．發布日期</span>
+            </div>
           </div>
           <!-- 單筆end -->
+                    <div class="article_one">
+            <!-- 收藏 -->
+            <div class="article_like">
+              <div class="num">
+                0
+              </div>
+              <div class="word">
+                like
+              </div>
+            </div>
+            <!-- 回覆 -->
+            <div class="article_reply">
+              <div class="num">
+                0
+              </div>
+              <div class="word">
+                回答
+              </div>
+            </div>
+            <!-- 文章 -->
+            <div class="article">
+              <div class="article_title">我是標題</div>
+              <span class="article_author">作者．發布日期</span>
+            </div>
+          </div>
 
         </div>
+        <!-- 頁數 -->
+        <el-button class="el-button--base datePickBtn" plain round >本月</el-button>
+
+        <el-pagination
+        v-model:currentPage="currentPage"
+        :page-size="10"
+        layout="prev, pager, next, jumper"
+        :total="pc_dataPage"
+        @current-change="handleCurrentChange"
+        >
+        </el-pagination>
+
       </div>
   </div>
 </template>
 
+
 <script>
 import { watch, onMounted, computed, ref, inject } from "vue";
 import store from "@/store/index.js";
+
 
 
 export default {
@@ -27,8 +91,23 @@ export default {
   components: {},
   setup() {
 
+    var currentPage = ref(1)
+    var pc_dataPage = ref(30)
+
+    onMounted(() => {
+
+
+
+    })
+
+    const handleCurrentChange = ()=>{
+
+    }
 
     return {
+      currentPage,
+      pc_dataPage,
+      handleCurrentChange
 
     };
   },
